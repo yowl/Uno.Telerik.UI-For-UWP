a389e5243719d5a07a8b7c1dbdf1423e22791598
Jérôme Laban <jerome.laban@nventive.com>
2018-01-08 14:34:28 -0500

    Add support for windows fonts, enable clipping feature.

--- SDKExamples.iOS/Main.cs
+++ SDKExamples.iOS/Main.cs
@@ -8,6 +8,9 @@ namespace SDKExamples.iOS
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
+			// Legacy clipping is enabled by default in Uno.UI 
+			Uno.UI.FeatureConfiguration.UIElement.UseLegacyClipping = false;
+
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, typeof(App));
